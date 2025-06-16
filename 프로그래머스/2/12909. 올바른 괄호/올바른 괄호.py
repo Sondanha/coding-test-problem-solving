7961cbab20d576efa5f3069f6bb993707b1ee001
Sondanha <155447715+Sondanha@users.noreply.github.com>
2025-06-16 16:15:52 +0900

    [level 2] Title: 올바른 괄호, Time: 7.95 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,11 @@
+def solution(s):
+    stack = []
+    for c in s:
+        if c == "(":
+            stack.append(c)
+        elif c == ")":
+            if not stack: 
+                return False
+            else: 
+                stack.pop()
+    return not stack
