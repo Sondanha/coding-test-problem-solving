aa30a0cc4224e10841edd36fbbf43a959606c410
Sondanha <155447715+Sondanha@users.noreply.github.com>
2025-07-04 16:10:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.13 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,12 +1,19 @@
+# def solution(participant, completion):
+#     sorted_parti = sorted(participant)
+#     sorted_compl = sorted(completion)
+        
+#     for idx, c in enumerate(sorted_compl):
+#         if c != sorted_parti[idx] and c == sorted_parti[idx+1]:
+#             return sorted_parti[idx]
+        
+#     return sorted_parti[-1]
+
 def solution(participant, completion):
     sorted_parti = sorted(participant)
     sorted_compl = sorted(completion)
         
-    for idx, c in enumerate(sorted_compl):
-        if c != sorted_parti[idx] and c == sorted_parti[idx+1]:
-            return sorted_parti[idx]
-        
+    for p, c in zip(sorted_parti, sorted_compl):
+        if p != c:
+            return p
     return sorted_parti[-1]
 
-# p = ['ana', 'mislav', 'mislav','mislav', 'stanko','stanko'] 
-# c = ['ana', 'mislav', 'mislav','stanko','stanko']
