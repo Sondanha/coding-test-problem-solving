74aa981838cc1c9f3e1e456a92af61fba7d95cb9
Sondanha <155447715+Sondanha@users.noreply.github.com>
2025-07-04 17:30:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 81.51 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,19 +1,14 @@
 # def solution(participant, completion):
 #     sorted_parti = sorted(participant)
 #     sorted_compl = sorted(completion)
-        
 #     for idx, c in enumerate(sorted_compl):
 #         if c != sorted_parti[idx] and c == sorted_parti[idx+1]:
-#             return sorted_parti[idx]
-        
+#             return sorted_parti[idx]  
 #     return sorted_parti[-1]
 
-def solution(participant, completion):
-    sorted_parti = sorted(participant)
-    sorted_compl = sorted(completion)
-        
-    for p, c in zip(sorted_parti, sorted_compl):
-        if p != c:
-            return p
-    return sorted_parti[-1]
+from collections import Counter 
 
+def solution(participant, completion):
+     
+    return list((Counter(participant)-Counter(completion)).keys())[0]
+    
