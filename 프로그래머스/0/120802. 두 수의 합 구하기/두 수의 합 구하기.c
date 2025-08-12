12dc783bffe8f259fbea0c773133707cb59edba6
Sondanha <155447715+Sondanha@users.noreply.github.com>
2025-08-12 17:28:43 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>   // 입출력 함수
+#include <stdbool.h> // bool, true, false 자료형
+#include <stdlib.h>  // 메모리, 변환, 난수 관련 함수
+
+int solution(int num1, int num2) {
+    int answer = num1 + num2;
+    return answer;
+}
+
+// int main() {
+//     int result = solution(3, 7);
+//     printf("%d\n", result);
+//     return 0;
+// }
